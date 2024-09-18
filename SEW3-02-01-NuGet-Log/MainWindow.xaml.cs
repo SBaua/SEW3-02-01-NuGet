844912eb5c3fca00f7e75fd42d5999b78bca8ee8
Simon Bauer <simon.bauer@josephinum.at>
2024-09-18 17:05:43 +0200

    logger programmiert

--- SEW3-02-01-NuGet-Log/MainWindow.xaml.cs
+++ SEW3-02-01-NuGet-Log/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using NLog;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,15 +17,44 @@ namespace SEW3_02_01_NuGet_Log
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        int z1;
+        int z2;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+       
+            
+        
+
         private void btn_add_Click(object sender, RoutedEventArgs e)
         {
-            double ergebnis = Convert.ToDouble(tbx_Eingabe_1.Text) + Convert.ToDouble(tbx_Eingabe_2.Text);
-            tbk_sum.Text = Convert.ToString(ergebnis); 
+            logger.Info("Button wurde betätigt");
+
+            try
+            {
+                logger.Info("Zahl 1, Zahl 2 konvertiert");
+                z1 = Convert.ToInt32(tbx_Eingabe_1.Text);
+                z2 = Convert.ToInt32(tbx_Eingabe_2.Text);
+            }
+
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Fehler beim Konvertieren der Zahlen");
+            }
+
+            try
+            {
+                logger.Info("Ergebnis konvertiert und ausgegeben");
+                tbk_sum.Text = Convert.ToString(z1 + z2);
+            }
+
+            catch
+            {
+                logger.Info("Fehler beim Konvertieren des Ergebnis");
+            }
         }
     }
 }
